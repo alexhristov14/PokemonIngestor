02378d94073f3157917a1cdf48d0133cd27e0553
APH <alex-hristov@hotmail.com>
2026-01-07 16:01:49 -0500

    refactor crawler

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -6,7 +6,12 @@
 import scrapy
 
 
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PokemonCard(scrapy.Item):
+    pokemon = scrapy.Field()
+    # card_num_in_set = scrapy.Field()
+    raw = scrapy.Field()
+    grade_7 = scrapy.Field()
+    grade_8 = scrapy.Field()
+    grade_9 = scrapy.Field()
+    grade_9_5 = scrapy.Field()
+    grade_10 = scrapy.Field()
--- crawler/crawler/spiders/pokechartspider.py
+++ crawler/crawler/spiders/pokechartspider.py
@@ -1,22 +1,43 @@
 import scrapy
+from crawler.items import PokemonCard
+import random
+import time
 
+USER_AGENTS = [
+    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36,gzip(gfe)",
+    "Mozilla/5.0 (Linux; Android 15; SM-S931B Build/AP3A.240905.015.A2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 15; SM-S931U Build/AP3A.240905.015.A2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/132.0.6834.163 Mobile Safari/537.36",
+    "Mozila/5.0 (Linux; Android 14; SM-S928B/DS) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.230 Mobile Safari/537.36",
+    "Mozila/5.0 (Linux; Android 14; SM-S928W) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.230 Mobile Safari/537.36",
+]
 
 class PokechartspiderSpider(scrapy.Spider):
     name = "pokechartspider"
     allowed_domains = ["pricecharting.com"]
-
-#   start_urls = [
-#           "https://www.pricecharting.com/game/pokemon-promo/oricorio-ex-24", 
-#           "https://www.pricecharting.com/game/pokemon-promo/mega-charizard-x-ex-23"
-#   ]
     
-    start_urls = ["https://www.pricecharting.com/console/pokemon-promo"]
+    start_urls = [
+        "https://www.pricecharting.com/category/pokemon-cards"
+    ]
 
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse)
+            yield scrapy.Request(
+                    url, 
+                    callback=self.parse,
+                    headers={'User-Agent': random.choice(USER_AGENTS)}
+            )
 
     def parse(self, response):
+        all_url_sets = response.xpath('//*[@id="home-page"]/div[4]/ul/li/a/@href').getall()
+
+        yield from response.follow_all(
+            all_url_sets,
+            callback=self.parse_set,
+        )
+
+    def parse_set(self, response):
         urls = response.xpath('//tr[starts-with(@id, "product-")]/td[2]/a/@href').getall() 
         
         yield from response.follow_all(
@@ -25,20 +46,14 @@ class PokechartspiderSpider(scrapy.Spider):
         )
 
     def parse_product(self, response):
-        pokemon = response.url.split("/")[-1]
-        raw = response.xpath('//td[@id="used_price"]/span[1]/text()').get()
-        grade_7 = response.xpath('//td[@id="complete_price"]/span[1]/text()').get()
-        grade_8 = response.xpath('//td[@id="new_price"]/span[1]/text()').get()
-        grade_9 = response.xpath('//td[@id="graded_price"]/span[1]/text()').get()
-        grade_9_5 = response.xpath('//td[@id="box_only_price"]/span[1]/text()').get()
-        grade_10 = response.xpath('//td[@id="manual_only_price"]/span[1]/text()').get()
+        item = PokemonCard()
 
-        yield {
-                "pokemon_name": pokemon,
-                "raw": raw.strip(),
-                "grade_7": grade_7.strip(),
-                "grade_8": grade_8.strip(),
-                "grade_9": grade_9.strip(),
-                "grade_9-5": grade_9_5.strip(),
-                "grade_10": grade_10.strip(),
-        }
+        item["pokemon"] = response.url.split("/")[-1]
+        item["raw"] = response.xpath('//td[@id="used_price"]/span[1]/text()').get().strip()
+        item["grade_7"] = response.xpath('//td[@id="complete_price"]/span[1]/text()').get().strip()
+        item["grade_8"] = response.xpath('//td[@id="new_price"]/span[1]/text()').get().strip()
+        item["grade_9"] = response.xpath('//td[@id="graded_price"]/span[1]/text()').get().strip()
+        item["grade_9_5"] = response.xpath('//td[@id="box_only_price"]/span[1]/text()').get().strip()
+        item["grade_10"] = response.xpath('//td[@id="manual_only_price"]/span[1]/text()').get().strip()
+
+        yield item
